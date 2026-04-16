c6a0aaa533bce2db079243726841f10ace7af7fa
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-04-16 16:08:13 -0600

    Update Ejercicio3Guia2.cs

--- Ejercicio3Guia2.cs
+++ Ejercicio3Guia2.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("RANGO DE ESTUDIANTES");
+        Console.WriteLine("Ingrese una letra (A, B, C, D y F)");
+        char letra = char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        switch (letra)
+        {
+            case 'A':
+                Console.WriteLine("EXCELENTE");
+                break;
+            case 'B':
+                Console.WriteLine("BUENO");
+                break;
+            case 'C':
+                Console.WriteLine("REGULAR");
+                break;
+            case 'D':
+                Console.WriteLine("DEFICIENTE");
+                break;
+            case 'F':
+                Console.WriteLine("REPROBADO");
+                break;
+            default:
+                Console.WriteLine("Escriba una letra valida!!!");
+                break;
+        }
+    }
+}
